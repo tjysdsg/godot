28756b9b498c89ee9e4769a2ab93e80a65a06606
Jiyang Tang <tjy1018543509@gmail.com>
2026-07-22 08:12:50 -0700

    Update build

--- build.ps1
+++ build.ps1
@@ -1,8 +1,14 @@
 ﻿# python misc/scripts/install_d3d12_sdk_windows.py
+param(
+  [switch]$vsproj
+)
+
+$vsprojOption = if ($vsproj) { "vsproj=yes" } else { "vsproj=no" }
+
 scons `
   platform=windows `
   d3d12=yes `
   production=yes `
   debug_symbols=yes `
+  $vsprojOption `
   -j8 `
-#   vsproj=yes
